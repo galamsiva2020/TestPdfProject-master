44e2ed492586a6543ade7c43e73d202781ecd291
krishna <krishna.desai240391@gmail.com>
2020-12-23 12:35:23 +0100

    First commit

---
+++ src/main/java/pool/ThreadPool.java
@@ -0,0 +1,38 @@
+package pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPool<T extends Convertor> {
+
+    private T convertor;
+    private ExecutorService executorService;
+    private boolean isStop;
+
+    public ThreadPool(T convertor) {
+        this.convertor = convertor;
+    }
+
+    public boolean isStop() {
+        return isStop;
+    }
+
+    public void start() {
+
+        executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                convertor.convert();
+            }
+        });
+    }
+
+    public void stop() {
+        executorService.shutdown();
+        this.isStop = executorService.isShutdown();
+    }
+
+}
